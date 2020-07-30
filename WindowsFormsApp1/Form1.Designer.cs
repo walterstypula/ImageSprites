a0d833f24e96f74190e14f0a4a6f790d5e9d77d0
Walter Stypula <walter.stypula@innroad.com>
2020-07-30 13:55:17 -0400

    feat: added quick and dirty winforms ui to with a listbox of images, and a filter option.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private ListBox lbSourceFiles;
+        private ListBox lbTarget;
+        private Button btnAdd;
+        private Button btnRemove;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btnSelectFolder;
+        private TextBox txtDirectory;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSourceFiles = new System.Windows.Forms.ListBox();
+            this.lbTarget = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSpriteName = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbSourceFiles
+            // 
+            this.lbSourceFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbSourceFiles.FormattingEnabled = true;
+            this.lbSourceFiles.Location = new System.Drawing.Point(3, 83);
+            this.lbSourceFiles.Name = "lbSourceFiles";
+            this.lbSourceFiles.Size = new System.Drawing.Size(256, 285);
+            this.lbSourceFiles.TabIndex = 0;
+            // 
+            // lbTarget
+            // 
+            this.lbTarget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbTarget.FormattingEnabled = true;
+            this.lbTarget.Location = new System.Drawing.Point(365, 83);
+            this.lbTarget.Name = "lbTarget";
+            this.lbTarget.Size = new System.Drawing.Size(256, 285);
+            this.lbTarget.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(87, 24);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(3, 33);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(87, 24);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lbSourceFiles, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtDirectory, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbTarget, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnSelectFolder, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(624, 421);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // txtDirectory
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtDirectory, 2);
+            this.txtDirectory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDirectory.Location = new System.Drawing.Point(265, 3);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(356, 20);
+            this.txtDirectory.TabIndex = 5;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSelectFolder.Location = new System.Drawing.Point(3, 3);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(256, 24);
+            this.btnSelectFolder.TabIndex = 5;
+            this.btnSelectFolder.Text = "select folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 3);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.txtSpriteName);
+            this.flowLayoutPanel1.Controls.Add(this.btnGenerate);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 374);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(618, 44);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "sprite name:";
+            // 
+            // txtSpriteName
+            // 
+            this.txtSpriteName.Location = new System.Drawing.Point(73, 3);
+            this.txtSpriteName.Name = "txtSpriteName";
+            this.txtSpriteName.Size = new System.Drawing.Size(199, 20);
+            this.txtSpriteName.TabIndex = 2;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(278, 3);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(201, 24);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate Sprite";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.btnAdd);
+            this.flowLayoutPanel2.Controls.Add(this.btnRemove);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(265, 83);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(94, 285);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFilter.Location = new System.Drawing.Point(3, 25);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(250, 20);
+            this.txtFilter.TabIndex = 4;
+            this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Filter";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.txtFilter, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 33);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(256, 44);
+            this.tableLayoutPanel2.TabIndex = 9;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 421);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private TextBox txtSpriteName;
+        private Button btnGenerate;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TextBox txtFilter;
+        private Label label2;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using Equin.ApplicationFramework;
+using ImageSprites;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private BindingListView<FileInfoDisplayName> sourceFiles;
+        private string[] extensions = new string[] { ".jpg", ".png", ".gif", ".jpeg" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lbSourceFiles.DisplayMember = "DisplayName";
+            lbTarget.DisplayMember = "DisplayName";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            var result = ofd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                var filePath = ofd.FileName;
+                var fileInfo = new FileInfo(filePath);
+
+                txtDirectory.Text = fileInfo.Directory.FullName;
+
+                var files = fileInfo.Directory.GetFiles("*", SearchOption.AllDirectories)
+                    .Where(f => extensions.Any(ext => ext == f.Extension))
+                    .Select(s =>
+                    {
+                        var displayName = s.FullName.Replace(txtDirectory.Text, "");
+                        return new FileInfoDisplayName() { File = s, DisplayName = displayName };
+                    }).ToList();
+
+                sourceFiles = new BindingListView<FileInfoDisplayName>(files);
+                lbSourceFiles.DataSource = sourceFiles;
+            }
+        }
+
+        private void txtFilter_TextChanged(object sender, EventArgs e)
+        {
+            Filter();
+        }
+
+        private void Filter()
+        {
+            var targetFiles = new List<FileInfoDisplayName>();
+            foreach (FileInfoDisplayName f in lbTarget.Items)
+            {
+                targetFiles.Add(f);
+            }
+
+            sourceFiles.ApplyFilter(f => f.File.Name.Contains(txtFilter.Text) && !targetFiles.Any(t => t.DisplayName == f.DisplayName));
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var item = (lbSourceFiles.SelectedItem as ObjectView<FileInfoDisplayName>)?.Object;
+
+            if (item == null)
+            {
+                return;
+            }
+
+            lbTarget.Items.Add(item);
+            Filter();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            var item = lbTarget.SelectedItem as FileInfoDisplayName;
+
+            if (item == null)
+            {
+                return;
+            }
+
+            var index = lbTarget.SelectedIndex;
+            lbTarget.Items.Remove(item);
+
+            while (index >= lbTarget.Items.Count)
+            {
+                index--;
+            }
+
+            lbTarget.SelectedIndex = index;
+
+            Filter();
+        }
+
+        private SpriteImageGenerator _generator = new SpriteImageGenerator();
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var _artifacts = new DirectoryInfo(".\\").FullName;
+
+            var spritesDirName = "Sprites";
+            if (!Directory.Exists(spritesDirName))
+            {
+                Directory.CreateDirectory(spritesDirName);
+            }
+
+            var _fileName = Path.Combine(_artifacts, spritesDirName, $"{txtSpriteName.Text}.sprite");
+
+            var images = new List<string>();
+            foreach (FileInfoDisplayName f in lbTarget.Items)
+            {
+                images.Add(f.File.FullName);
+            }
+
+            var document = new SpriteDocument(_fileName, images);
+            document.Orientation = ImageSprites.Orientation.Horizontal;
+            document.Padding = 20;
+            document.Output = ImageType.Png;
+            document.Stylesheet = Stylesheet.Css;
+            document.CustomStyles["display"] = "block";
+            document.CustomStyles.Add("margin", "0");
+
+            var task = Task.Run(() => _generator.Generate(document));
+            task.Wait();
+
+            var documentTask = Task.Run(() => document.Save());
+            documentTask.Wait();
+        }
+    }
+
+    public class FileInfoDisplayName
+    {
+        public string DisplayName { get; set; }
+
+        public FileInfo File { get; set; }
+    }
+}
